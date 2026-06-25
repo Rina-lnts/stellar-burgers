d8785004542a6d8e1f5134d3278731b62a1e7b0b
Ekaterina Elenets <elenets.katy@gmail.com>
2026-06-25 16:22:58 +0300

    burger_2

--- src/services/slices/feedSlice.ts
+++ src/services/slices/feedSlice.ts
@@ -63,8 +63,7 @@ const feedSlice = createSlice({
 export const selectFeedOrders = (state: RootState) => state.feed.orders;
 export const selectUserOrders = (state: RootState) => state.feed.userOrders;
 export const selectFeedTotal = (state: RootState) => state.feed.total;
-export const selectFeedTotalToday = (state: RootState) =>
-  state.feed.totalToday;
+export const selectFeedTotalToday = (state: RootState) => state.feed.totalToday;
 export const selectFeedLoading = (state: RootState) => state.feed.isLoading;
 
 export default feedSlice.reducer;
